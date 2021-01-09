ec733c123de20fab9cc7d8f9e8e4cc09897ff19a
이재헌 <shmom467@hanmail.net>
2021-01-09 09:33:33 +0000

    How to use Overriding

--- index.py
+++ index.py
@@ -1,9 +1,49 @@
-a = "Life is too short, you need python"
+# add.mul.sub.abs
+# Fourcal
+# setdata
 
-if "wife" in a: print("wife")
-elif "python" in a and "you" not in a: print("python")
-elif "shirt" not in a: print("shirt")
-elif "need" in a: print("need")
-else: print("none")
-    
-    
+
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+
+    def add(self):
+        return self.first + self.second
+
+    def mul(self):
+        return self.first * self.second
+
+    def sub(self):
+        return self.first - self.second
+
+    def div(self):
+        return self.first / self.second
+
+
+class MoreFourCal(FourCal):
+    pass
+
+
+class SafeFourCal(FourCal):
+    def div(self):
+        if self.second == 0:
+            return 0
+        else:
+            return self.first / self.second
+
+
+a = MoreFourCal(3, 4)
+print(a.first)
+print(a.second)
+
+a.setdata(1, 0)
+
+print(a.first)
+print(a.second)
+
+print(a.sub())
